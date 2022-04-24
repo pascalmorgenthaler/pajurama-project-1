cd38028e349f033010e5e6796d9b6da8077c5eae
pascalmorgenthaler <morgenthalerpascal@gmail.com>
2022-04-24 11:50:26 +0200

    added some animate on scroll

--- _site/main.js
+++ _site/main.js
@@ -1,10 +1,17 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
 
-
-$('.hamburger-button').click(function(){
+  $('.hamburger-button').click(function(){
   
     $('.mob-menubar').slideToggle();
     
   });
+
+  // Animate on scroll / activate
+  AOS.init();
+
+});
+
 
 
 
